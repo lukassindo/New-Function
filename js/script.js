d4aa521fbc1e0ea1f96f7c8945f01b3c47368acf
Lukasz Trzaska <lukasztrzaska81@gmail.com>
2018-01-04 19:07:14 +0100

    poprawki

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 
+
 function getTriangleArea(a,h) {
 	if ( (a > 0) && (h > 0) ) {
-	var area = a*h/2;
-} 
-	else  {
-	alert('Nieprawidłowe dane');
+		return a*h/2;
+	} 
+	else {
+		console.log('Nieprawidłowe dane');
 	}
-    return area;
 }
 
 console.log(getTriangleArea(10,6));
 
 var Triangle1Area = getTriangleArea(3,5);
-var Triangle2Area = getTriangleArea(8,9);
+var Triangle2Area = getTriangleArea(8,0);
 var Triangle3Area = getTriangleArea(87,2);
 
+console.log('Powierzchnia trójkąta pierwszego: ' + Triangle1Area);
+console.log('Powierzchnia trójkąta drugiego: ' + Triangle2Area);
+console.log('Powierzchnia trójkąta trzeciego: ' + Triangle3Area);
 
 
